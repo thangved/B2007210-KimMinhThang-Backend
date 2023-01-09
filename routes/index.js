880046e6c39a2c1646d734b71f3690f261b4f54c
Kim Minh Thang <thangved02@gmail.com>
2023-01-09 11:25:23 +0700

    Fix buf

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const { Router } = require("express");
 
+const AuthMiddleware = require("~/middleware/auth.middleware");
+
 const router = Router();
 
-router.use("/contacts", require("./contact.route"));
+router.use(
+	"/contacts",
+	AuthMiddleware,
+	require("./contact.route")
+);
 router.use("/auth", require("./auth.route"));
 
 module.exports = router;
